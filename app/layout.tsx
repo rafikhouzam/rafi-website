7b86a88247a8ec8c488f520412af6afe3283dde8
Rafi Khouzam <rafi.khouzam@bc.edu>
2025-08-24 20:42:45 -0400

    bug: removed SEOClient that was breaking npm run build

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import HashRedirector from "./HashRedirector";
 import type { Metadata } from "next";
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
-import SEOClient from "./SEOClient"; // client wrapper for next-seo (below)
 
 export const metadata: Metadata = {
   title: "Rafi Khouzam — Data Engineer & Builder",
@@ -24,7 +23,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="bg-[url('/terrazo_background.svg')] bg-no-repeat bg-cover bg-fixed">
         <div className="min-h-screen bg-white/85 dark:bg-slate-950/80">
         <HashRedirector />
-        <SEOClient />
         <Nav />
         <main className="container py-10 md:py-14">{children}</main>
         <Footer />
